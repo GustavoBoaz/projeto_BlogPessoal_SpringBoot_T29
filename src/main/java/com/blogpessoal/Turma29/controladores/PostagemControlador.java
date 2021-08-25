36dfd0263ceb27199c2dd886860c1953a22aa30c
Gustavo Boaz <gustavo.boaz@hotmail.com>
2021-08-25 15:03:36 -0300

    [Feature Boaz - Task: CRUD Postagem]

--- src/main/java/com/blogpessoal/Turma29/controladores/PostagemControlador.java
+++ src/main/java/com/blogpessoal/Turma29/controladores/PostagemControlador.java
@@ -1,10 +1,20 @@
 package com.blogpessoal.Turma29.controladores;
 
 import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.blogpessoal.Turma29.modelos.Postagem;
@@ -13,12 +23,66 @@ import com.blogpessoal.Turma29.repositorios.PostagemRepositorio;
 @RestController
 @RequestMapping("/api/v1/postagem")
 public class PostagemControlador {
-	
+
 	private @Autowired PostagemRepositorio repositorio;
-	
+
 	@GetMapping("/todas")
-	public List<Postagem> pegarTodes(){
-		return repositorio.findAll();
+	public ResponseEntity<List<Postagem>> pegarTodos() {
+		List<Postagem> objetoLista = repositorio.findAll();
+
+		if (objetoLista.isEmpty()) {
+			return ResponseEntity.status(204).build();
+		} else {
+			return ResponseEntity.status(200).body(objetoLista);
+		}
+	}
+
+	@PostMapping("/salvar")
+	public ResponseEntity<Postagem> salvar(@Valid @RequestBody Postagem novaPostagem) {
+		return ResponseEntity.status(201).body(repositorio.save(novaPostagem));
+	}
+
+	@GetMapping("/{id_postagem}")
+	public ResponseEntity<Postagem> buscarPorId(@PathVariable(value = "id_postagem") Long idPostagem) {
+		Optional<Postagem> objetoPostagem = repositorio.findById(idPostagem);
+
+		if (objetoPostagem.isPresent()) {
+			return ResponseEntity.status(200).body(objetoPostagem.get());
+		} else {
+			return ResponseEntity.status(204).build();
+		}
+	}
+
+	@GetMapping("/titulo/{titulo}")
+	public ResponseEntity<List<Postagem>> buscarPorTituloI(@PathVariable(value = "titulo") String titulo) {
+		List<Postagem> objetoLista = repositorio.findAllByTituloContainingIgnoreCase(titulo);
+
+		if (objetoLista.isEmpty()) {
+			return ResponseEntity.status(204).build();
+		} else {
+			return ResponseEntity.status(200).body(objetoLista);
+		}
+	}
+
+	@GetMapping("/pesquisa")
+	public ResponseEntity<List<Postagem>> buscarPorTituloII(@RequestParam(defaultValue = "") String titulo) {
+		List<Postagem> objetoLista = repositorio.findAllByTituloContainingIgnoreCase(titulo);
+
+		if (objetoLista.isEmpty()) {
+			return ResponseEntity.status(204).build();
+		} else {
+			return ResponseEntity.status(200).body(objetoLista);
+		}
+	}
+
+	@PutMapping("/atualizar")
+	public ResponseEntity<Postagem> atualizar(@Valid @RequestBody Postagem postagemParaAtualizar) {
+		return ResponseEntity.status(201).body(repositorio.save(postagemParaAtualizar));
+	}
+
+	@DeleteMapping("/deletar/{id_postagem}")
+	public void deletarTituloPorId(@PathVariable(value = "id_postagem") Long idPostagem) {
+		repositorio.deleteById(idPostagem);
 	}
 
 }
--- src/main/java/com/blogpessoal/Turma29/repositorios/PostagemRepositorio.java
+++ src/main/java/com/blogpessoal/Turma29/repositorios/PostagemRepositorio.java
@@ -1,5 +1,7 @@
 package com.blogpessoal.Turma29.repositorios;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,14 @@ import com.blogpessoal.Turma29.modelos.Postagem;
 
 @Repository
 public interface PostagemRepositorio extends JpaRepository<Postagem, Long> {
+
+	/**
+	 * Método utilizado para pesquisar coluna titulo ContainigIgnoreCase
+	 * 
+	 * @param titulo do tipo String
+	 * @return List de Postagens
+	 * @author Turma 29
+	 */
+	List<Postagem> findAllByTituloContainingIgnoreCase(String titulo);
 
 }
