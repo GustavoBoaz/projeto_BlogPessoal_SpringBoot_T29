2359b2e2d936c8d769cd0652211dfbad846031bf
Gustavo Boaz <gustavo.boaz@hotmail.com>
2021-08-25 11:50:13 -0300

    [Feature Boaz - Task: CRUD Usuario]

--- src/main/java/com/blogpessoal/Turma29/controladores/UsuarioControlador.java
+++ src/main/java/com/blogpessoal/Turma29/controladores/UsuarioControlador.java
@@ -1,10 +1,20 @@
 package com.blogpessoal.Turma29.controladores;
 
 import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.blogpessoal.Turma29.modelos.Usuario;
@@ -15,9 +25,64 @@ import com.blogpessoal.Turma29.repositorios.UsuarioRepositorio;
 public class UsuarioControlador {
 
 	private @Autowired UsuarioRepositorio repositorio;
-	
+
 	@GetMapping("/todes")
-	public List<Usuario> pegarTodes(){
-		return repositorio.findAll();
+	public ResponseEntity<List<Usuario>> pegarTodes() {
+		List<Usuario> objetoLista = repositorio.findAll();
+
+		if (objetoLista.isEmpty()) {
+			return ResponseEntity.status(204).build();
+		} else {
+			return ResponseEntity.status(200).body(objetoLista);
+		}
+
+	}
+
+	@PostMapping("/salvar")
+	public ResponseEntity<Usuario> salvar(@Valid @RequestBody Usuario novoUsuario) {
+		return ResponseEntity.status(201).body(repositorio.save(novoUsuario));
+	}
+	
+	@GetMapping("/{id_usuario}")
+	public ResponseEntity<Usuario> buscarPorId(@PathVariable(value = "id_usuario") Long idUsuario){
+		Optional<Usuario> objetoUsuario = repositorio.findById(idUsuario);
+		
+		if (objetoUsuario.isPresent()) {
+			return ResponseEntity.status(200).body(objetoUsuario.get());
+		} else {
+			return ResponseEntity.status(204).build();
+		}
+	}
+	
+	@GetMapping("/nome/{nome_usuario}")
+	public ResponseEntity<List<Usuario>> buscarPorNomeI(@PathVariable(value = "nome_usuario") String nome){
+		List<Usuario> objetoLista = repositorio.findAllByNomeContainingIgnoreCase(nome);
+		
+		if (objetoLista.isEmpty()) {
+			return ResponseEntity.status(204).build();
+		} else {
+			return ResponseEntity.status(200).body(objetoLista);
+		}
+	}
+	
+	@GetMapping("/pesquisa")
+	public ResponseEntity<List<Usuario>> buscarPorNomeII(@RequestParam(defaultValue = "") String nome){
+		List<Usuario> objetoLista = repositorio.findAllByNomeContainingIgnoreCase(nome);
+		
+		if (objetoLista.isEmpty()) {
+			return ResponseEntity.status(204).build();
+		} else {
+			return ResponseEntity.status(200).body(objetoLista);
+		}
+	}
+	
+	@PutMapping("/atualizar")
+	public ResponseEntity<Usuario> atualizar(@Valid @RequestBody Usuario usuarioParaAtualizar) {
+		return ResponseEntity.status(201).body(repositorio.save(usuarioParaAtualizar));
+	}
+	
+	@DeleteMapping("/deletar/{id_usuario}")
+	public void deletarUsuarioPorId(@PathVariable(value = "id_usuario") Long idUsuario) {
+		repositorio.deleteById(idUsuario);
 	}
 }
--- src/main/java/com/blogpessoal/Turma29/repositorios/UsuarioRepositorio.java
+++ src/main/java/com/blogpessoal/Turma29/repositorios/UsuarioRepositorio.java
@@ -1,5 +1,7 @@
 package com.blogpessoal.Turma29.repositorios;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,14 @@ import com.blogpessoal.Turma29.modelos.Usuario;
 
 @Repository
 public interface UsuarioRepositorio extends JpaRepository<Usuario, Long> {
+	
+	/**
+	 * Método utilizado para pesquisar coluna nome ContainigIgnoreCase
+	 * 
+	 * @param nome do tipo String
+	 * @return List de Usuarios
+	 * @author Turma 29
+	 */
+	List<Usuario> findAllByNomeContainingIgnoreCase(String nome);
 
 }
