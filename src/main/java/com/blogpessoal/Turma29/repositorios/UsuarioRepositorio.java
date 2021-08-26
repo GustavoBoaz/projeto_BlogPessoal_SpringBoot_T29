d2666d119349b11fb9cd57c8c2d2c923d9a63473
Gustavo Boaz <gustavo.boaz@hotmail.com>
2021-08-26 10:37:32 -0300

    [Feature Boaz - Task: create method findByEmail]

--- src/main/java/com/blogpessoal/Turma29/repositorios/UsuarioRepositorio.java
+++ src/main/java/com/blogpessoal/Turma29/repositorios/UsuarioRepositorio.java
@@ -1,6 +1,7 @@
 package com.blogpessoal.Turma29.repositorios;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -16,7 +17,20 @@ public interface UsuarioRepositorio extends JpaRepository<Usuario, Long> {
 	 * @param nome do tipo String
 	 * @return List de Usuarios
 	 * @author Turma 29
+	 * @since 1.0
+	 * 
 	 */
 	List<Usuario> findAllByNomeContainingIgnoreCase(String nome);
+	
+	/**
+	 * Método utilizado para pesquisar coluna email
+	 * 
+	 * @param email do tipo String
+	 * @return Optional com Usuario
+	 * @author Turma 29
+	 * @since 1.0
+	 * 
+	 */
+	Optional<Usuario> findByEmail(String email);
 
 }
