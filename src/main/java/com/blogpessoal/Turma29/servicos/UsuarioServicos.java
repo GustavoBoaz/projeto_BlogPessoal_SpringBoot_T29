594f096d26cb197d7a41384ea5fe9ad62c10d894
Gustavo Boaz <gustavo.boaz@hotmail.com>
2021-09-03 10:17:21 -0300

    [Feature Boaz - Task: ADD Cripto]

--- src/main/java/com/blogpessoal/Turma29/servicos/UsuarioServicos.java
+++ src/main/java/com/blogpessoal/Turma29/servicos/UsuarioServicos.java
@@ -3,6 +3,7 @@ package com.blogpessoal.Turma29.servicos;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.blogpessoal.Turma29.modelos.Usuario;
@@ -12,20 +13,24 @@ import com.blogpessoal.Turma29.repositorios.UsuarioRepositorio;
 public class UsuarioServicos {
 
 	private @Autowired UsuarioRepositorio repositorio;
-	
+
 	/**
-	 * Método utilizado para criação de um novo usuario no sistema
+	 * Método utilizado para criação de um novo usuario no sistema e criptografia da
+	 * senha
 	 * 
 	 * @param novoUsuario do tipo Usuario
 	 * @return Optional com Usuario Criado
 	 * @author Turma 29
-	 * @since 1.0
+	 * @since 1.5
 	 * 
 	 */
 	public Optional<Object> cadastrarUsuario(Usuario novoUsuario) {
 		return repositorio.findByEmail(novoUsuario.getEmail()).map(usuarioExistente -> {
 			return Optional.empty();
 		}).orElseGet(() -> {
+			BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+			String result = encoder.encode(novoUsuario.getSenha());
+			novoUsuario.setSenha(result);
 			return Optional.ofNullable(repositorio.save(novoUsuario));
 		});
 	}
