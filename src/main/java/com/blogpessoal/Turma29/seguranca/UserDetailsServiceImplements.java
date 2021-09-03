1b8af75c9d7007002fc7cb8806d5e252823deec8
Gustavo Boaz <gustavo.boaz@hotmail.com>
2021-09-03 11:53:26 -0300

    [Feature Boaz - Task: ADD UserDetails and UserDetailsService | Implements]

---
+++ src/main/java/com/blogpessoal/Turma29/seguranca/UserDetailsServiceImplements.java
@@ -0,0 +1,30 @@
+package com.blogpessoal.Turma29.seguranca;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.blogpessoal.Turma29.modelos.Usuario;
+import com.blogpessoal.Turma29.repositorios.UsuarioRepositorio;
+
+@Service
+public class UserDetailsServiceImplements implements UserDetailsService {
+	
+	private @Autowired UsuarioRepositorio repositorio;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		Optional<Usuario> objetoOptional = repositorio.findByEmail(username);
+		
+		if (objetoOptional.isPresent()) {
+			return new UserDetailsImplements(objetoOptional.get());			
+		} else {
+			throw new UsernameNotFoundException(username + "Não existe!");
+		}
+	}
+
+}
