7191a8d2a4fd5a02a1f645745e9e5fccec882b62
Gustavo Boaz <gustavo.boaz@hotmail.com>
2021-09-03 11:21:14 -0300

    [Feature Boaz - Task: ADD method pegarCredenciais]

--- src/main/java/com/blogpessoal/Turma29/servicos/UsuarioServicos.java
+++ src/main/java/com/blogpessoal/Turma29/servicos/UsuarioServicos.java
@@ -1,12 +1,15 @@
 package com.blogpessoal.Turma29.servicos;
 
+import java.nio.charset.Charset;
 import java.util.Optional;
 
+import org.apache.commons.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.blogpessoal.Turma29.modelos.Usuario;
+import com.blogpessoal.Turma29.modelos.utilidades.UsuarioDTO;
 import com.blogpessoal.Turma29.repositorios.UsuarioRepositorio;
 
 @Service
@@ -34,8 +37,40 @@ public class UsuarioServicos {
 			return Optional.ofNullable(repositorio.save(novoUsuario));
 		});
 	}
-	
-	public Optional<?> pegarCredenciais(){
-		return null;
+
+	/**
+	 * Metodo utilizado para pegar credenciais do usuario com Tokem (Formato Basic),
+	 * este método sera utilizado para retornar ao front o token utilizado para ter
+	 * acesso aos dados do usuario e mantelo logado no sistema
+	 * 
+	 * @param usuarioParaAutenticar do tipo UsuarioLoginDTO necessario email e senha
+	 *                              para validar
+	 * @return UsuarioLoginDTO preenchido com informações mais o Token
+	 * @since 1.0
+	 * @author Turma 29
+	 */
+	public Optional<?> pegarCredenciais(UsuarioDTO usuarioParaAutenticar) {
+		return repositorio.findByEmail(usuarioParaAutenticar.getEmail()).map(usuarioExistente -> {
+			BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+			if (encoder.matches(usuarioParaAutenticar.getSenha(), usuarioExistente.getSenha())) {
+
+				String estruturaBasic = usuarioParaAutenticar.getEmail() + ":" + usuarioParaAutenticar.getSenha(); // gustavoboaz@gmail.com:134652
+				byte[] autorizacaoBase64 = Base64.encodeBase64(estruturaBasic.getBytes(Charset.forName("US-ASCII"))); // hHJyigo-o+i7%0ÍUG465sas=-
+				String autorizacaoHeader = "Basic " + new String(autorizacaoBase64); // Basic hHJyigo-o+i7%0ÍUG465sas=-
+
+				usuarioParaAutenticar.setToken(autorizacaoHeader);
+				usuarioParaAutenticar.setId(usuarioExistente.getIdUsuario());
+				usuarioParaAutenticar.setNome(usuarioExistente.getNome());
+				usuarioParaAutenticar.setSenha(usuarioExistente.getSenha());
+				return Optional.ofNullable(usuarioParaAutenticar); // Usuario Credenciado
+
+			} else {
+				return Optional.empty(); // Senha esteja incorreta
+			}
+
+		}).orElseGet(() -> {
+			return Optional.empty(); // Email não existente
+		});
 	}
 }
