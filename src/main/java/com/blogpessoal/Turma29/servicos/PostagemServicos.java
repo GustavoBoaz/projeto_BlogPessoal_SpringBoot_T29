2964ab4a9cb0d0cebcb740bfd9c5797ed0111675
Cristiano-Silva-Soares <criztianosoares99@outlook.com>
2021-09-11 21:30:14 -0300

    [Feature Cristiano - Task: Update na classe UsuarioServicos por meio da adição de trataiva de erro no método Salvar Postagem]

---
+++ src/main/java/com/blogpessoal/Turma29/servicos/PostagemServicos.java
@@ -0,0 +1,50 @@
+package com.blogpessoal.Turma29.servicos;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.blogpessoal.Turma29.modelos.Postagem;
+import com.blogpessoal.Turma29.modelos.Tema;
+import com.blogpessoal.Turma29.repositorios.PostagemRepositorio;
+import com.blogpessoal.Turma29.repositorios.TemaRepositorio;
+import com.blogpessoal.Turma29.repositorios.UsuarioRepositorio;
+
+@Service
+public class PostagemServicos {
+
+	private @Autowired PostagemRepositorio repositorioP;
+	private @Autowired UsuarioRepositorio repositorioU;
+	private @Autowired TemaRepositorio repositorioT;
+
+	/**
+	 * Método usado no cadastro de uma nova postagem dentro do banco, validando se o
+	 * usuario criador é existente. O id do usuario criador e o id do tema devem ser
+	 * repassados dentro do objeto postagem para que a devida criação seja efetuada. Caso o id
+	 * do usuario não for passado ou não existir no banco, há retorno de um
+	 * Optional.empty()
+	 * 
+	 * @param novaPostagem do tipo Postagem
+	 * @return Optional com Postagem
+	 * @since 1.5
+	 * @author Turma 29
+	 */
+
+	public Optional<?> cadastrarPostagem(Postagem novaPostagem) {
+		Optional<Tema> objetoExistente = repositorioT.findById(novaPostagem.getTemaRelacionado().getIdTema());
+		return repositorioU.findById(novaPostagem.getCriador().getIdUsuario()).map(usuarioExistente -> {
+			if (objetoExistente.isPresent()) {
+				novaPostagem.setCriador(usuarioExistente);
+				novaPostagem.setTemaRelacionado(objetoExistente.get());
+				return Optional.ofNullable(repositorioP.save(novaPostagem));
+			} else {
+				return Optional.empty();
+
+			}
+		}).orElseGet(() -> {
+			return Optional.empty();
+
+		});
+	}
+}
