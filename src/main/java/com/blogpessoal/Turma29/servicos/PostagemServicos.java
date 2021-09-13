a1026c171fbcb79feb327fb1e5eb52f8cd0b0e31
Gustavo Boaz <gustavo.boaz@hotmail.com>
2021-09-13 10:11:28 -0300

    [Feature Boaz - Task: UPDATE method atualizarPostagem]

--- src/main/java/com/blogpessoal/Turma29/servicos/PostagemServicos.java
+++ src/main/java/com/blogpessoal/Turma29/servicos/PostagemServicos.java
@@ -18,31 +18,31 @@ public class PostagemServicos {
 	private @Autowired UsuarioRepositorio repositorioU;
 	private @Autowired TemaRepositorio repositorioT;
 
-	/*
-	 * Método utilizado para alterar uma postagem que retorna um Optional com Postagem
-	 * caso corretoou um Optional.empty() caso id da Postagem não exista.
+	/**
+	 * Método utilizado para alterar uma postagem que retorna um Optional com
+	 * Postagem caso corretoou um Optional.empty() caso id da Postagem não exista.
 	 * 
 	 * @param postagemParaAlterar do tipo Postagem
 	 * @return Optional com Postagem alterada
-	 * @since 1.0
+	 * @since 1.5
 	 * @author Turma 29
 	 */
-		public Optional<Postagem> atualizarPostagem(Postagem postagemParaAlterar){
-			return repositorio.findById(postagemParaAlterar.getIdPostagem()).map(postagemExistente->{
-				postagemExistente.setTitulo(postagemParaAlterar.getTitulo());
-				postagemExistente.setDescricao(postagemParaAlterar.getDescricao());
-				return Optional.ofNullable(repositorio.save(postagemExistente));
-			}).orElseGet(() ->{
-				return Optional.empty();
-			});
-		}
+	public Optional<Postagem> atualizarPostagem(Postagem postagemParaAlterar) {
+		return repositorioP.findById(postagemParaAlterar.getIdPostagem()).map(postagemExistente -> {
+			postagemExistente.setTitulo(postagemParaAlterar.getTitulo());
+			postagemExistente.setDescricao(postagemParaAlterar.getDescricao());
+			return Optional.ofNullable(repositorioP.save(postagemExistente));
+		}).orElseGet(() -> {
+			return Optional.empty();
+		});
+	}
 
 	/**
 	 * Método usado no cadastro de uma nova postagem dentro do banco, validando se o
 	 * usuario criador é existente. O id do usuario criador e o id do tema devem ser
-	 * repassados dentro do objeto postagem para que a devida criação seja efetuada. Caso o id
-	 * do usuario não for passado ou não existir no banco, há retorno de um
-	 * Optional.empty()
+	 * repassados dentro do objeto postagem para que a devida criação seja efetuada.
+	 * Caso o id do usuario não for passado ou não existir no banco, há retorno de
+	 * um Optional.empty()
 	 * 
 	 * @param novaPostagem do tipo Postagem
 	 * @return Optional com Postagem
