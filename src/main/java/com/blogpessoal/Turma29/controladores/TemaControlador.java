ea916f04b39db17cf9d4a4c90ec0d57a8a6afd41
Gustavo Boaz <gustavo.boaz@hotmail.com>
2021-09-14 15:19:08 -0300

    [Feature Boaz - Task: ADD notation CrossOrigin]

--- src/main/java/com/blogpessoal/Turma29/controladores/TemaControlador.java
+++ src/main/java/com/blogpessoal/Turma29/controladores/TemaControlador.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,6 +24,7 @@ import com.blogpessoal.Turma29.servicos.TemaServicos;
 
 @RestController
 @RequestMapping("/api/v1/tema")
+@CrossOrigin("*")
 public class TemaControlador {
 
 	private @Autowired TemaRepositorio repositorio;
--- src/main/java/com/blogpessoal/Turma29/controladores/UsuarioControlador.java
+++ src/main/java/com/blogpessoal/Turma29/controladores/UsuarioControlador.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,6 +25,7 @@ import com.blogpessoal.Turma29.servicos.UsuarioServicos;
 
 @RestController
 @RequestMapping("/api/v1/usuario")
+@CrossOrigin("*")
 public class UsuarioControlador {
 
 	private @Autowired UsuarioRepositorio repositorio;
